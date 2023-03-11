7fc58b8ae7b5bbd08f5f8ff1f6eb44b915085dca
Alkhansaaa <114305721+Alkhansaaa@users.noreply.github.com>
2023-03-11 17:25:20 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,31 @@
+import 'package:tick/screens/admin/home_screen_A.dart';
+import 'package:tick/screens/acount/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'firebase_options.dart';
+import 'constants.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tickey',
+      theme: ThemeData(
+        backgroundColor: const Color.fromARGB(255, 248, 248, 248),
+        primaryColor: kPraimaryColor,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const LoginScreen(),
+    );
+  }
+}
